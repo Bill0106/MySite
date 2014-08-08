e58d8d0efe32447a09c61faabf0c92c367bf023b
zhuhaolin <zhuhaolin@verycd.com>
2014-08-08 14:05:11 +0800

    Set up home links API

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,31 @@
  * Created by Bill on 14-8-8.
  */
 
+var isAuthenticated = require('../config/authenticate');
+
 module.exports = function(app, router)
 {
+    router.use(isAuthenticated, function(req, res, next)
+    {
+        next();
+    });
+
+    // Home Links API Route
+    var homeLinks = require('../config/homelinks');
+
+    router.route('/homelinks')
+        .get(function(req, res)
+        {
+            res.json(homeLinks);
+        });
+
+    // Apply API Routes
+    app.use('/api', router);
+
     // Fronted Route
     app.route('*')
         .get(function(req, res)
         {
-            res.send('Hello World!');
+            res.sendfile('./public/views/layout.html');
         });
-}
+};
---
+++ config/homelinks.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Bill on 14-8-8.
+ */
+
+var homeLinks = {};
+
+homeLinks.playStation = {};
+homeLinks.playStation.url = '/playstation';
+homeLinks.playStation.title = 'PlayStation';
+homeLinks.playStation.icon = 'c34742cdcfb3963c35ef875f4d7cc498.png';
+homeLinks.playStation.background = '120711a3d24da9f734ecfa2241490b9e.jpg';
+
+homeLinks.saiban = {};
+homeLinks.saiban.url = '/saiban';
+homeLinks.saiban.title = 'Gyakuten Saiban';
+homeLinks.saiban.icon = '58f762846a467be673164f96409b4938.png';
+homeLinks.saiban.background = '412b40983a332870e1fee97c3499a561.jpg';
+
+homeLinks.gourmet = {};
+homeLinks.gourmet.url = '/gourmet';
+homeLinks.gourmet.title = 'Gourmet Tour';
+homeLinks.gourmet.icon = 'c8af5750b24cf75fd99ce1a222129136.png';
+homeLinks.gourmet.background = '5ea8a973077ba3bcc9e1520400a47bcb.jpg';
+
+homeLinks.manutd = {};
+homeLinks.manutd.url = '/manutd';
+homeLinks.manutd.title = 'Manchester United';
+homeLinks.manutd.icon = '3c4e720bf023f64d69cf0d1b37822735.png';
+homeLinks.manutd.background = 'd1ecc379966e7911ae8eb3da99dac769.jpg';
+
+homeLinks.gundam = {};
+homeLinks.gundam.url = '/gundam';
+homeLinks.gundam.title = 'Mobile Suit Gundam';
+homeLinks.gundam.icon = '8f87e2c4380cc5c531f95083d47fb4b4.png';
+homeLinks.gundam.background = 'aeaeda9a3fd45b903663e928ebded3fd.jpg';
+
+homeLinks.batman = {};
+homeLinks.batman.url = '/batman';
+homeLinks.batman.title = 'Batman';
+homeLinks.batman.icon = 'b6c9fa2117b0f7087ab405709d471a32.png';
+homeLinks.batman.background = '0dc4f026ce55e9d07bdde0ab4dfba27a.jpg';
+
+module.exports = homeLinks;
