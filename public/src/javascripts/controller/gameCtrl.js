943ab60f9669c95959b0f1f7e2a51d932943c2f2
Bill <bill2010111@gmail.com>
2014-09-11 18:41:28 +0800

    Set up game page

--- public/src/javascripts/controller/gameCtrl.js
+++ public/src/javascripts/controller/gameCtrl.js
@@ -8,6 +8,18 @@ gameCtrl.controller('gameController', function($scope, $routeParams, Game)
 {
     Game.get($routeParams.url).success(function(data)
     {
-        console.log(data.name);
+        $scope.game = data;
+
+        $scope.descriptions = data.description.split("\n");
+
+        var companies = data.company.split("/");
+
+        if (companies.length == '1') {
+            $scope.developer = data.company;
+            $scope.publish = data.company;
+        } else {
+            $scope.developer = companies[0];
+            $scope.publish = companies[1];
+        }
     });
 });
