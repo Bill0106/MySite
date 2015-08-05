f70603f9c566a7589cce210a844403e895c3ab22
bill <zhuhaolin@verycd.com>
2015-08-05 13:03:00 +0800

    Recode Games Page

--- angular/app.js
+++ angular/app.js
@@ -19,3 +19,64 @@ myApp.run(function($rootScope, $state, $http)
         $rootScope.title = $state.current.title;
     });
 });
+
+myApp.directive('ngLoading', function()
+{
+    return {
+        restrict: 'A',
+        replace: true,
+        scope: {
+            val: '=loadImages'
+        },
+        link: function(scope, element, attrs)
+        {
+            function progressIncrease(total)
+            {
+                var mask = $("[data-load='mask']");
+                var progress = $("[data-load='progress']", mask);
+                var content = $("[data-load='content']");
+                var count = progress.data('count');
+
+                count++;
+                var percentage = Math.floor((count / total) * 100);
+                progress
+                    .css('width', percentage + '%')
+                    .attr('aria-valuenow', percentage)
+                    .text(percentage + '%')
+                    .data('count', count);
+
+                if (count == total) {
+                    content.removeClass('hidden');
+                    setTimeout(function()
+                    {
+                        mask.fadeOut();
+                    }, 300);
+                }
+            }
+
+            function loadImage(item, total)
+            {
+                var image = new Image();
+                var path = scope.$root.imagePath;
+                var src = path + item;
+
+                $(image).attr('src', src).bind('load', function()
+                {
+                    progressIncrease(total);
+                });
+            }
+
+            scope.$watch('val', function(newValue, oldValue)
+            {
+                if (newValue) {
+                    var total = scope.val.length;
+
+                    angular.forEach(scope.val, function(value)
+                    {
+                        loadImage(value, total);
+                    });
+                }
+            });
+        }
+    };
+});
--- angular/controllers/app/gamesController.js
+++ angular/controllers/app/gamesController.js
@@ -9,21 +9,16 @@ gamesController.controller('gamesController', function($scope, Game)
     Game.query(function(data)
     {
         $scope.games = data;
+
+        $scope.images = [];
+        angular.forEach(data, function(value)
+        {
+            $scope.images.push(value.image);
+        });
     });
 
     $scope.getNumber = function(num)
     {
         return new Array(num);
-    };
-
-    $scope.getDate = function(timestamp)
-    {
-        var newDate = new Date(parseInt(timestamp));
-        var year = newDate.getFullYear();
-        var month = newDate.getMonth();
-        var day = newDate.getDate();
-        var date = year + '.' + month + '.' + day;
-
-        return date;
     };
 });
