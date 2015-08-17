84a79667affade7ac568042a5f1149cdb52f27ae
bill <zhuhaolin@verycd.com>
2015-08-17 12:14:02 +0800

    Bug Fix

--- app/controllers/games.js
+++ app/controllers/games.js
@@ -39,34 +39,68 @@ exports.find = function(req, res)
 
 exports.create = function(req, res)
 {
-    games.findOne({ _id: req.body._id }, function(err, data)
-    {
-        if (!data) {
-            data = new games();
-        }
+    if (req.body._id) {
+        games.findOne({ _id: req.body._id }, function(err, data)
+        {
+            data.title       = req.body.title;
+            data.name        = req.body.name;
+            data.publisher   = req.body.publisher;
+            data.developer   = req.body.developer;
+            data.release_at  = timestamp(req.body.release_at);
+            data.buy_at      = timestamp(req.body.buy_at);
+            data.rate        = req.body.rate;
+            data.image       = req.body.image;
+            data.url         = req.body.name.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+            data.platform    = req.body.platform;
+            data.genre       = req.body.genre;
+            data.description = req.body.description;
 
-        data.title       = req.body.title;
-        data.name        = req.body.name;
-        data.publisher   = req.body.publisher;
-        data.developer   = req.body.developer;
-        data.release_at  = timestamp(req.body.release_at);
-        data.buy_at      = timestamp(req.body.buy_at);
-        data.rate        = req.body.rate;
-        data.image       = req.body.image;
-        data.url         = req.body.name.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
-        data.platform    = req.body.platform;
-        data.genre       = req.body.genre;
-        data.description = req.body.description;
+            if (req.body.url) {
+                data.url = req.body.url;
+            }
+
+            data.save(function(error)
+            {
+                var result = {
+                    "success": true,
+                    "msg": data._id
+                };
+
+                if (error) {
+                    result = {
+                        "success": false,
+                        "msg": error
+                    }
+                }
+
+                res.json(result);
+            });
+        });
+    } else {
+        var game = new games();
+
+        game.title       = req.body.title;
+        game.name        = req.body.name;
+        game.publisher   = req.body.publisher;
+        game.developer   = req.body.developer;
+        game.release_at  = timestamp(req.body.release_at);
+        game.buy_at      = timestamp(req.body.buy_at);
+        game.rate        = req.body.rate;
+        game.image       = req.body.image;
+        game.url         = req.body.name.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+        game.platform    = req.body.platform;
+        game.genre       = req.body.genre;
+        game.description = req.body.description;
 
         if (req.body.url) {
-            data.url = req.body.url;
+            game.url = req.body.url;
         }
 
-        data.save(function(error)
+        game.save(function(error)
         {
             var result = {
                 "success": true,
-                "msg": data._id
+                "msg": game._id
             };
 
             if (error) {
@@ -78,5 +112,5 @@ exports.create = function(req, res)
 
             res.json(result);
         });
-    });
+    }
 };
