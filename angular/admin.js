4f2cc3218a5a1c53c70420ade47e735ff5d4200d
bill <zhuhaolin@verycd.com>
2016-04-23 19:13:15 +0800

    Optimize Admin Pages

--- angular/admin.js
+++ angular/admin.js
@@ -38,4 +38,11 @@ angular.module('myAdmin', [
         };
 
         return this;
-    }]);
+    }])
+    .filter('capitalize', function()
+    {
+        return function (str)
+        {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+    });
