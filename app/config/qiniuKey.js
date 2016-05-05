87fea3fda9a730f07ea1c4c8c067961f408c0449
bill <zhuhaolin@verycd.com>
2016-05-05 13:43:15 +0800

    Qiniu Image Upload Command

---
+++ app/config/qiniuKey.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bill on 16/4/26.
+ */
+
+var key = {
+    "AK": 'JlQP_xR9XmOPV54i6PapLYifQ5lzI9aRCkHECW5n',
+    "SK": 'n6Vv7U-rg7PY9TcDqm5crCUgqMBv5JMJxKpSkzuN'
+};
+
+module.exports = key;
