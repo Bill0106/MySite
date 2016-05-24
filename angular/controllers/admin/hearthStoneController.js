dba441740d9dbb37acc5f7e23574d1828200d69b
bill <zhuhaolin@verycd.com>
2016-05-24 13:50:04 +0800

    Optimize Code

--- angular/controllers/admin/hearthStoneController.js
+++ angular/controllers/admin/hearthStoneController.js
@@ -294,6 +294,19 @@ angular.module('hearthStoneAdmin', [])
             return true;
         };
     })
+    .filter('range', function ()
+    {
+        return function(input, min, max) {
+            min = parseInt(min);
+            max = parseInt(max);
+
+            for (var i = min; i < max; i++) {
+                input.push(i);
+            }
+
+            return input;
+        };
+    })
     .service('hearthStoneCards', function (HSCard)
     {
         this.getCards = function (playerClass)
