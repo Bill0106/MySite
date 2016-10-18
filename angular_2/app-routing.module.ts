65280cc990eaf34aacc0c69478d40b19a87578c9
bill <zhuhaolin@verycd.com>
2016-10-18 10:44:01 +0800

    Optimize App Routing

---
+++ angular_2/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home.component';
+// import { GamesComponent } from './components/games.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    // { path: 'games', component: GamesComponent }
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
--- angular_2/app.module.ts
+++ angular_2/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app.component';
 import { HomeComponent } from './components/home.component';
+// import { GamesComponent } from './components/games.component';
 
-import { routing } from './app.routing';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     imports:      [
         BrowserModule,
-        routing
+        HttpModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        // GamesComponent
     ],
     bootstrap   : [ AppComponent ]
 })
