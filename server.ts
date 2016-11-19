04c80f78c435416399d768b08de2227d05af383e
bill <bill2010111@gmail.com>
2016-11-19 22:34:11 +0800

    Use koi-send for Frontend Page

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import * as Koa from 'koa';
 import * as mongoose from 'mongoose';
 import * as server from 'koa-static';
+import * as send from 'koa-send';
 
 import router from './koa/routes';
 
+(<any>mongoose).Promise = global.Promise;
 mongoose.connect('mongodb://localhost/database');
 mongoose.connection.on('error', console.error);
 
@@ -21,6 +23,15 @@ app
     .use(server(__dirname + '/public'))
     .use(router.routes())
     .use(router.allowedMethods())
+    .use(async (ctx, next) => {
+        if (ctx.path.indexOf('/api') === 0) {
+            next();
+        } else if (ctx.path.indexOf('/admin') === 0) {
+            await send(ctx, 'admin.html', { root: __dirname + '/public' });
+        } else {
+            await send(ctx, 'index.html', { root: __dirname + '/public' });
+        }
+    })
 
 app.listen(8888);
 
