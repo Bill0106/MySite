ff6f3b8cbf4d8f8bfc5836bb83f398d869c7dc30
bill <bill2010111@gmail.com>
2016-11-20 23:36:23 +0800

    Set Up Game Api

---
+++ koa/controllers/game.controller.ts
@@ -0,0 +1,102 @@
+import * as moment from 'moment';
+
+import * as Game from '../models/game.model';
+
+const list = async (ctx) => {
+    try {
+        let limit = parseInt(ctx.query.limit) || 20;
+        let page = parseInt(ctx.query.page) || 1;
+        let skip = limit * (page - 1);
+
+        let games = await Game.repositry.find().limit(limit).skip(skip).sort({ buy_at: 'desc' }).sort({ release_at: 'desc' });
+        for (let i = 0; i < games.length; i++) {
+            games[i].title = new Buffer(games[i].title, 'base64').toString();
+        }
+
+        ctx.body = {
+            list: games,
+            total: await Game.repositry.count({}),
+        };
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const find = async (ctx) => {
+    try {
+        let game = await Game.repositry.findOne({ url: ctx.params.url });
+        game.title = new Buffer(game.title, 'base64').toString();
+        ctx.body = game;
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const creat = async (ctx) => {
+    try {
+        let data = ctx.request.body;
+
+        data.title = new Buffer(data.title).toString('base64');
+        data.release_at = moment(data.release_at, 'YYYY-MM-DD').valueOf();
+        data.buy_at = moment(data.buy_at, 'YYYY-MM-DD').valueOf();
+        data.url = data.url || data.name.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+
+        let game = new Game.repositry(data);
+        await game.save();
+        
+        ctx.body = {
+            success: true,
+            data: {
+                id: game._id
+            }
+        }
+    } catch (error) {
+        ctx.status = error.status || 500;
+        ctx.body = {
+            success: false,
+            error: error.message,
+        }
+    }
+}
+
+const update = async (ctx) => {
+    try {
+        let data = ctx.request.body;
+
+        data.title = new Buffer(data.title).toString('base64');
+        data.release_at = moment(data.release_at, 'YYYY-MM-DD').valueOf();
+        data.buy_at = moment(data.buy_at, 'YYYY-MM-DD').valueOf();
+        data.url = data.url || data.name.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+
+        await Game.repositry.findByIdAndUpdate(data.id, data);
+        
+        ctx.body = {
+            success: true,
+            data: {
+                id: data.id
+            }
+        }
+    } catch (error) {
+        ctx.status = error.status || 500;
+        ctx.body = {
+            success: false,
+            error: error.message,
+        }
+    }
+}
+
+const remove = async (ctx) => {
+    try {
+        await Game.repositry.findByIdAndRemove(ctx.request.body.id);
+    } catch (error) {
+        ctx.status = error.status || 500;
+        ctx.body = {
+            success: false,
+            error: error.message,
+        }
+    }
+}
+
+export default { list, find, creat, update, remove }
---
+++ koa/models/game.model.ts
@@ -0,0 +1,33 @@
+import * as mongoose from 'mongoose';
+
+export interface IGame extends mongoose.Document {
+    title       : string;
+    name        : string;
+    publisher   : string;
+    developer   : string;
+    release_at  : number;
+    buy_at      : number;
+    rate        : number;
+    image       : string;
+    url         : string;
+    platform    : number;
+    genre       : number;
+    description : number;
+}
+
+export let gameSchema = new mongoose.Schema({
+    title       : String,
+    name        : String,
+    publisher   : String,
+    developer   : String,
+    release_at  : Number,
+    buy_at      : Number,
+    rate        : Number,
+    image       : String,
+    url         : String,
+    platform    : Number,
+    genre       : Number,
+    description : String
+});
+
+export let repositry = mongoose.model<IGame>("Game", gameSchema);
