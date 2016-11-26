17d81a2a4cc7e56b8f610972a1a046941d755f7c
bill <bill2010111@gmail.com>
2016-11-26 13:53:46 +0800

    Hearthstone Season API

--- koa/controllers/count.controller.ts
+++ koa/controllers/count.controller.ts
@@ -1,5 +1,6 @@
 import * as Game from '../models/game.model';
 import * as Gourmet from '../models/gourmet.model';
+import * as HearthstoneSeason from '../models/hearthstone-season.model';
 
 const list = async (ctx, next) => {
     try {
@@ -11,6 +12,10 @@ const list = async (ctx, next) => {
             {
                 table: 'Gourmets',
                 count: await Gourmet.repositry.count({})
+            },
+            {
+                table: 'Hearthstone Seasons',
+                count: await HearthstoneSeason.repositry.count({})
             }
         ]
     } catch (error) {
---
+++ koa/controllers/hearthstone-season.controller.ts
@@ -0,0 +1,82 @@
+import * as moment from 'moment';
+
+import * as HearthstoneSeason from '../models/hearthstone-season.model';
+
+const list = async (ctx) => {
+    try {
+        let limit = parseInt(ctx.query.limit) || 12;
+        let page = parseInt(ctx.query.page) || 1;
+        let skip = limit * (page - 1);
+
+        ctx.body = {
+            list: await HearthstoneSeason.repositry.find().limit(limit).skip(skip).sort({ month: 'desc' }),
+            total: await HearthstoneSeason.repositry.count({}),
+        };
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const find = async (ctx) => {
+    try {
+        ctx.body = await HearthstoneSeason.repositry.findOne({ url: ctx.params.url });
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const create = async (ctx) => {
+    try {
+        let data = ctx.request.body;
+        data.month = moment(data.month, 'YYYY-MM-DD').startOf('month').startOf('day').valueOf();
+        data.url = data.url || data.title.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+        
+        let season = new HearthstoneSeason.repositry(data);
+        await season.save();
+        ctx.body = {
+            success: true,
+            data: {
+                id: season._id
+            }
+        }
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const update = async (ctx) => {
+    try {
+        let data = ctx.request.body;
+        data.month = moment(data.month, 'YYYY-MM-DD').startOf('month').startOf('day').valueOf();
+        data.url = data.url || data.title.toLowerCase().replace(/ /g, '-').replace(/:/g, '');
+
+        await HearthstoneSeason.repositry.findByIdAndUpdate(data.id, data);
+        ctx.body = {
+            success: true,
+            data: {
+                id: data.id
+            }
+        }
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+const remove = async (ctx) => {
+    try {
+        await HearthstoneSeason.repositry.findByIdAndRemove(ctx.request.body.id);
+
+        ctx.body = {
+            success: true,
+        }
+    } catch (error) {
+        ctx.body = error.message;
+        ctx.status = error.status || 500;
+    }
+}
+
+export default { list, find, create, update, remove }
