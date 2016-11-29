a6803bf478a6ee753621492f0e32c7262b861e7c
bill <bill2010111@gmail.com>
2016-11-29 19:34:18 +0800

    Hearthstone Card Update Command

--- config/keys.ts
+++ config/keys.ts
@@ -11,7 +11,8 @@ const KEYS = {
         "AK": 'SJbrypZAGXKxpSGWLe4k2t1ThqM8IFZaT1S0fVTq',
         "SK": 'mKzfOxYLVf_PHBxq2Oj4RGIxW4kvhlEDz9DL9gYj',
         "domain": 'http://7xtddu.com1.z0.glb.clouddn.com/'
-    }
+    },
+    "mashape": 'm8Ydu9cOBmmshaqxQorKcaMLZEDHp1oT2iQjsnwlXYOsZidTWi'
 }
 
 export let Keys = KEYS;
---
+++ koa/commands/hearthstone-cards-update.command.ts
@@ -0,0 +1,74 @@
+import * as mongoose from 'mongoose';
+import axios from 'axios';
+
+import * as Card from '../models/hearthstone-card.model';
+
+import { Keys } from '../../config/keys';
+import { HsPlayerClasses } from '../../config/hs-player-classes';
+import { HsCardRarity } from '../../config/hs-card-rarity';
+
+const CARD_TYPE = ['Spell', 'Minion', 'Weapon'];
+const CARD_SET = ['Naxxramas', 'Goblins vs Gnomes'];
+
+const handle = async () => {
+    console.log('Requesting API ...');
+    try {
+        const data = await requestAPI();
+        let cards = [];
+        await Object.keys(data).map(key => {
+            if (data[key].length) {
+                cards = cards.concat(data[key]);
+            }
+        });
+        cards = cards.filter(card => CARD_TYPE.indexOf(card.type) >= 0);
+
+        (<any>mongoose).Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/database');
+        mongoose.connection.on('error', console.error);
+
+        let updated = 0;
+        let created = 0;
+        for (let item of cards) {
+            let newData = {
+                cardId: item.cardId,
+                name: item.name,
+                image: item.img,
+                cost: item.cost,
+                playerClass: item.playerClass ? HsPlayerClasses.find(player => player.name == item.playerClass).value : -1,
+                rarity: HsCardRarity.find(rarity => rarity.name == item.rarity).value,
+            }
+
+            let card = await Card.repositry.findOne({ cardId: newData.cardId });
+            if (card) {
+                await card.update(newData);
+                updated++;
+                console.log(newData.name, 'Updated!');
+            } else {
+                await new Card.repositry(newData).save();
+                created++;
+                console.log(newData.name, 'Created!');
+            }
+        }
+
+        console.log('Total:', cards.length, 'Updated:', updated, 'Created:', created);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function requestAPI(): any {
+    return new Promise((resolve, reject) => {
+        axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1', {
+            headers: {
+                "X-Mashape-Key": Keys.mashape
+            }
+        }).then(response => {
+            resolve(response.data);
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+handle();
