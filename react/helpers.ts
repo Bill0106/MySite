b5e168f8f5b085488b3f241d42461ce373dd86bd
bill <bill2010111@gmail.com>
2016-12-28 16:03:12 +0800

    Optimize Code

--- react/helpers.ts
+++ react/helpers.ts
@@ -4,15 +4,15 @@ export function time2Date(timestamp: number, displayTime = false) {
     }
 
     const time = new Date(timestamp);
-    let year = time.getFullYear();
-    let month = (time.getMonth() + 1);
-    let day = time.getDate();
-    let hour = time.getHours();
-    let minute = time.getMinutes();
-    let second = time.getSeconds();
+    const year = time.getFullYear();
+    const month = (time.getMonth() + 1);
+    const day = time.getDate();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
 
-    let arr = [month, day, hour, minute, second];
-    let newArr = arr.map(t => ('0' + t).slice(-2));
+    const arr = [month, day, hour, minute, second];
+    const newArr = arr.map(t => ('0' + t).slice(-2));
     
     const dateArray = [year.toString()].concat(newArr.slice(0, 2));
     const timeArray = newArr.slice(2);
